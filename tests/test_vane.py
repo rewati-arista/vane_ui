49e0e6c33933b12d6f925599c8235148dde23b37
kgrozis <kgrozis@arista.com>
2020-10-21 10:45:45 -0400

    added simple unit test for pipeline

--- tests/test_vane.py
+++ tests/test_vane.py
@@ -1,4 +1,10 @@
-import vane
+import vane.bin.tests_client as tests_client
 
 def test_assert():
     assert True
+
+def test_object_creation():
+    """ Verify spreadsheet can be inputted
+    """
+
+    tc = tests_client.TestsClient('/project/vane/bin/definitions.yaml')
