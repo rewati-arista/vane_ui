1c25cfabb55beb65768eed3d13c80a1d3f1e1752
kgrozis <kgrozis@arista.com>
2020-10-23 12:39:36 -0400

    addded tests for method _set_test_parameters

--- tests/test_tests_client.py
+++ tests/test_tests_client.py
@@ -41,41 +41,98 @@ def test_setting_test_parameters():
     """ Validate that test parametters are settable based on a definition file
     """
 
-    verbosity_levels = [False, True]
-    stdout_levels = [True, False]
-    testcases = ["All", "evpn", None]
-    html_reports = [None, "../reports/report"]
+    definitions = {
+        'verbose': [False, True],
+        'stdout': [True, False],
+        'test_cases': ["All", "evpn", None],
+        'html_report': [None, "report"],
+        'excel_report': ["report", None],
+        'json_report': [None, "report"],
+        'processes': [3, 2, 1, None],
+        'setup_show': [False, True],
+        'mark': ['nrfu', 'cpu', 'memory', None]
+    }
 
-    # Test verbosity level can be changed
-    for verbosity_level in verbosity_levels:
-        TC.data_model['parameters']['verbose'] = verbosity_level
-        test_parameters = TC._set_test_parameters()
-        assert verbosity_level == ('-v' in test_parameters)
+    extensions = {
+        'verbose': '-v',
+        'stdout': '-s',
+        'setup_show': '--setup-show',
+        'test_cases': '-k',
+        'html_report': 'html',
+        'json_report': 'json',
+        'excel_report': 'excel',
+        'processes': '-n',
+        'mark': '-m',
+    }
 
-    # Test stdout level can be changed
-    for stdout_level in stdout_levels:
-        TC.data_model['parameters']['stdout'] = stdout_level
-        test_parameters = TC._set_test_parameters()
-        assert stdout_level == ('-s' in test_parameters)
-    
-    # Test testcase specific entries
-    for testcase in testcases:
-        TC.data_model['parameters']['test_cases'] = testcase
-        test_parameters = TC._set_test_parameters()
+    processes = [3, 2, 1, None]
+    marks = ['nrfu', 'cpu', 'memory', None]
 
-        if testcase == 'All':
-            assert False == ('-k' in test_parameters)
-        else:
-            assert True == (f'-k {testcase}' in test_parameters)
+    for definition in definitions:
+        report_dir = TC.data_model['parameters']['report_dir']
 
-    # for html_report in html_reports:
-    #     TC.data_model['parameters']['html_report'] = html_report
-    #     test_parameters = TC._set_test_parameters()
-# 
-    #     if html_report:
-    #         assert True == (f'--html={html_report}.html' in test_parameters)
-    #     else:
-    #         html_expr = [for x in ]
+        if definition in ['verbose', 'stdout', 'setup_show']:
+            for definition_value in definitions[definition]:
+                TC.data_model['parameters'][definition] = definition_value
+                test_parameters = TC._set_test_parameters()
+
+                assert definition_value == (extensions[definition] in test_parameters)
+
+        elif definition in ['test_cases']:
+            for definition_value in definitions[definition]:
+                TC.data_model['parameters'][definition] = definition_value
+                test_parameters = TC._set_test_parameters()
+                extension = extensions[definition]
+
+                if definition_value == 'All' or not definition_value:
+                    assert False == (extension in test_parameters)
+                else:
+                    assert True == (f'{extension} {definition_value}' in test_parameters)
+        
+        elif definition in ['html_report', 'json_report', 'excel_report']:
+            for definition_value in definitions[definition]:
+                TC.data_model['parameters'][definition] = definition_value
+                test_parameters = TC._set_test_parameters()
+
+                if extensions[definition] == 'excel':
+                    extension = f'--{extensions[definition]}report'
+                    suffix = 'xlsx'
+                else:
+                    extension = f'--{extensions[definition]}'
+                    suffix = extensions[definition]
+
+                if definition_value:
+                    assert True == (f'{extension}={report_dir}/{definition_value}.{suffix}' in test_parameters)
+                else:
+                    list_output = [x for x in test_parameters if extension in x]
+                    assert True == (len(list_output) == 0)
+
+        elif definition in ['processes', 'mark']:
+            for definition_value in definitions[definition]:
+                TC.data_model['parameters'][definition] = definition_value
+                test_parameters = TC._set_test_parameters()
+                extension = extensions[definition]
+
+                if definition_value:
+                    assert True == (f'{extension} {definition_value}' in test_parameters)
+                else:
+                    list_output = [x for x in test_parameters if extension in x]
+                    assert True == (len(list_output) == 0)
+
+def test_test_parameters_not_set():
+    """ Validate that test parametters are settable based on a definition file
+    """
+
+    definitions = ['verbose', 'stdout', 'test_cases', 'html_report', 
+                   'excel_report', 'json_report', 'processes', 'mark',
+                   'setup_show']
+    tc = tests_client.TestsClient(DEFINITIONS)
+
+    for definition in definitions:
+        _ = tc.data_model['parameters'].pop(definition, 1)
+        test_parameters = tc._set_test_parameters()
+        print(f'>>>>>>> {test_parameters}')
+        assert None == tc.data_model['parameters'][definition]
 
 def test_import_no_definitions():
     """ Test script exits if spreadsheet doesn't exist
