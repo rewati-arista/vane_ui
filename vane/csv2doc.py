df9d67a8a4ed6766aceec5674aff5869524949ab
kgrozis <kgrozis@arista.com>
2023-03-03 17:09:26 -0500

    additional updates to output spreadsheet

--- vane/csv2doc.py
+++ vane/csv2doc.py
@@ -4,6 +4,7 @@ import docx
 from docx.shared import Inches, Pt, RGBColor, Mm
 from docx.oxml.ns import qn, nsdecls
 from docx.oxml import OxmlElement, parse_xml
+from docx.enum.text import WD_COLOR_INDEX
 
 # Input Variables
 # Name of CSV to use for test output
@@ -18,7 +19,9 @@ PASS_FAIL = 13 # column N
 OBSERVATION = 14 # column O
 
 def parse_csv():
+    print(f"Parsing CSV file: {CSVFILE} for test data...")
     test_data = []
+
     # open csv
     with open(CSVFILE, 'r') as file:
         csv_reader = csv.reader(file)
@@ -46,6 +49,9 @@ def parse_csv():
     return test_data
 
 def write_report(test_data):
+    print(f"Writing report doc: {DOCFILE}")
+
+    # Open doc
     document = docx.Document()
     # Change margins
     section = document.sections[0]
@@ -54,7 +60,14 @@ def write_report(test_data):
 
     write_header(document)
     write_results_table(document, test_data)
-    write_footer(document)
+
+    doc_text = (
+        "\n\n\nPlease note – for more details on test outputs please reference "
+        "the test case output appendix docs.\n"
+    )
+    write_text(document, doc_text)
+
+    # Close doc
     document.save(DOCFILE)
 
 def write_header(document):
@@ -69,16 +82,10 @@ def write_header(document):
         "Please note – for more details on test outputs please reference "
         "the test case output appendix docs.\n\n\n"
     )
-    run = para.add_run(doc_text)
-    run.font.size = Pt(10)
-    run.font.name = "Arial"
+    write_text(document, doc_text)
 
-def write_footer(document):
+def write_text(document, doc_text):
     para = document.add_paragraph()
-    doc_text = (
-        "\n\n\nPlease note – for more details on test outputs please reference "
-        "the test case output appendix docs.\n"
-    )
     run = para.add_run(doc_text)
     run.font.size = Pt(10)
     run.font.name = "Arial"
@@ -86,44 +93,78 @@ def write_footer(document):
 def write_results_table(document, test_data):
     headers = ["Test Case", "Case Name", "Procedure", "Expected result", "Pass/Fail", "Observation"]
     table = document.add_table(rows=1, cols=6, style="Table Grid")
-    set_col_widths(table)
-
+    
     for count, header in enumerate(headers):
         para = table.rows[0].cells[count].paragraphs[0]
-        tourquise = parse_xml(
-            r'<w:shd {} w:fill="OOFFFF"/>'.format(nsdecls("w"))
-        )
-        table.rows[0].cells[count]._tc.get_or_add_tcPr().append(tourquise)
+        
+        cell = table.cell(0, count)
         run = para.add_run(header)
         run.font.name = "Arial"
         run.font.size = Pt(9)
-    
+        run.font.bold = True
+
+        cell = table.cell(0, count)
+        shading_blue = parse_xml(r'<w:shd {} w:fill="00FFFF"/>'.format(nsdecls('w')))
+        cell._tc.get_or_add_tcPr().append(shading_blue)
+
+
     pattern = r"\d{1,2}\.\d{1,2}"
 
     for count, test_case_entry in enumerate(test_data):
         count += 1
-        print(f"comparing {test_case_entry[0]}")
 
         if re.match(pattern, test_case_entry[0]):
             row_cells = table.add_row().cells
-            # table.cell(count, 0).merge(table.cell(count, 1))
-            # first_cell = table.cell(count, 0)
-            # second_cell = table.cell(count, 1)
             merged_cell = row_cells[0].merge(row_cells[5])
-            #row_cells[0].text = test_case_entry
             row_cells[0].text = ''
             run = row_cells[0].paragraphs[0].add_run(test_case_entry)
             run.font.name = "Arial"
             run.font.size = Pt(9)
             run.bold = True
+
+            cell = table.cell(count, 0)
+            shading_blue = parse_xml(r'<w:shd {} w:fill="CCFFFF"/>'.format(nsdecls('w')))
+            cell._tc.get_or_add_tcPr().append(shading_blue)
+
         elif "TN" in test_case_entry[0]:
             row_cells = table.add_row().cells
+
             for counter, test_data_entry in enumerate(test_case_entry):
-                print(f"writing cell {counter} with {test_data_entry}")
                 row_cells[counter].text = ''
-                run = row_cells[counter].paragraphs[0].add_run(test_data_entry)
+                run.font.name = "Arial"
+                run.font.size = Pt(9)  
+
+                if counter == 0:
+                    test_data_entry = format_test_id(test_data_entry)
+                    run = row_cells[counter].paragraphs[0].add_run(test_data_entry)
+                elif counter == 4:
+                    run = row_cells[counter].paragraphs[0].add_run(test_data_entry.upper())
+
+                    if test_data_entry.upper() == "PASS":
+                        run.font.highlight_color = WD_COLOR_INDEX.BRIGHT_GREEN
+                        run.font.bold = True
+                    elif test_data_entry.upper() == "FAIL":
+                        run.font.highlight_color = WD_COLOR_INDEX.RED
+                        run.font.bold = True
+                    else:
+                        run.font.highlight_color = WD_COLOR_INDEX.YELLOW
+                        run.font.bold = True
+                else:
+                    run = row_cells[counter].paragraphs[0].add_run(test_data_entry)
+
                 run.font.name = "Arial"
                 run.font.size = Pt(9)
+
+    # set_col_widths(table)
+        
+def format_test_id(test_data_entry):
+    if "TN" in test_data_entry:
+        test_data_entry = test_data_entry.split("TN")[1]
+
+    return test_data_entry
+
+def format_pass_fail(test_data_entry):
+    pass
 
 def set_col_widths(table):
     widths = (Inches(.5), Inches(1.17), Inches(1.55), Inches(1.25), Inches(.85), Inches(2.15))
@@ -132,8 +173,10 @@ def set_col_widths(table):
             row.cells[idx].width = width
 
 def main():
+    print("Starting...")
     test_data = parse_csv()
     write_report(test_data)
+    print("Complete")
 
 if __name__ == "__main__":
     main()
