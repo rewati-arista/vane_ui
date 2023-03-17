88eba2586e5534f0a3d9e2c90f637e62c6afa0b8
Shachi Agarwal <sagarwal@arista.com>
2023-03-17 10:29:05 -0600

    Add network configs to dut fixture

--- vane/tests_tools.py
+++ vane/tests_tools.py
@@ -372,6 +372,10 @@ def login_duts(test_parameters, test_duts):
     duts = test_duts["duts"]
     logins = []
     eapi_file = test_parameters["parameters"]["eapi_file"]
+    network_configs = {}
+    if network_configs in test_parameters["parameters"]:
+        if test_parameters["parameters"]["network_configs"]:
+            network_configs = import_yaml(test_parameters["parameters"]["network_configs"]:
     for dut in duts:
         name = dut["name"]
         login_index = len(logins)
@@ -400,7 +404,8 @@ def login_duts(test_parameters, test_duts):
         login_ptr["results_dir"] = test_parameters["parameters"]["results_dir"]
         login_ptr["report_dir"] = test_parameters["parameters"]["report_dir"]
         login_ptr["eapi_file"] = eapi_file
-
+        if network_configs.has_key(name):
+            login_ptr["network_configs"] = network_configs[name]
     logging.info(f"Returning duts logins: {logins}")
     return logins
 
