c566c7f6be50aca082c5ad1c2b0b9d649e7137c1
Rewati Shitole <rewati@arista.com>
2023-04-20 15:48:49 -0700

    Refactored test_filesystem.py as per test guide

--- sample_network_tests/filesystem/test_filesystem.py
+++ sample_network_tests/filesystem/test_filesystem.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019, Arista Networks EOS+
+# Copyright (c) 2023, Arista Networks EOS+
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -29,10 +29,12 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
-""" Tests to validate base feature status."""
+""" Testcases to validate filesystem."""
 
 import pytest
+from pyeapi.eapilib import EapiError
 from vane import tests_tools
+from vane.vane_logging import logging
 
 
 TEST_SUITE = __file__
@@ -50,7 +52,7 @@ class FileSystemTests:
     """EOS File System Test Suite"""
 
     def test_if_files_on_(self, dut, tests_definitions):
-        """Verify filesystem is correct and expected files are present
+        """TD: Verify filesystem is correct and expected files are present
 
         Args:
           dut (dict): Encapsulates dut details including name, connection
@@ -62,27 +64,53 @@ class FileSystemTests:
         files = tops.test_parameters["files"]
 
         for file_name in files:
-            show_cmd = f"show file information {file_name}"
-            show_output = tops.run_show_cmds([show_cmd])
-            tops.actual_output = show_output[0]["result"]["isDir"]
+            try:
+                """TS: Run show command 'show file information' on dut"""
 
-            tops.output_msg += (
-                f"\nOn router |{tops.dut_name}|: {file_name} file isDir "
-                f"state is |{tops.actual_output}|, correct state is "
-                f"|{tops.expected_output}|.\n"
-            )
+                show_cmd = f"show file information {file_name}"
+                self.output = tops.run_show_cmds([show_cmd])
+                assert self.output, "File information not collected."
+                logging.info(
+                    f"On device {tops.dut_name} output of {show_cmd} command is: {self.output}"
+                )
 
-            tops.test_result = tops.actual_output is tops.expected_output
+                tops.actual_output = self.output[0]["result"]["isDir"]
+
+            except (
+                AssertionError,
+                AttributeError,
+                LookupError,
+                EapiError,
+            ) as exception:
+                logging.error(
+                    f"""Error occurred during the testsuite execution on dut: {tops.dut_name}
+                    is {str(exception)}"""
+                )
+                tops.actual_output = str(exception)
+
+            if tops.actual_output == tops.expected_output:
+                tops.test_result = True
+                tops.output_msg += (
+                    f"\nOn router |{tops.dut_name}|: {file_name} file isDir "
+                    f"state is |{tops.actual_output}| which is the correct state.\n"
+                )
+
+            else:
+                tops.test_result = False
+                tops.output_msg += (
+                    f"\nOn router |{tops.dut_name}|: {file_name} file isDir "
+                    f"state is |{tops.actual_output}|, while the correct state is "
+                    f"|{tops.expected_output}|.\n"
+                )
 
             tops.actual_results.append(tops.actual_output)
             tops.expected_results.append(tops.expected_output)
-
-        print(f"{tops.output_msg}\n{tops.comment}")
 
         tops.actual_output, tops.expected_output = (
             tops.actual_results,
             tops.expected_results,
         )
-        tops.generate_report(tops.dut_name, tops.output_msg)
 
+        tops.parse_test_steps(self.test_if_files_on_)
+        tops.generate_report(tops.dut_name, self.output)
         assert tops.actual_results == tops.expected_results
