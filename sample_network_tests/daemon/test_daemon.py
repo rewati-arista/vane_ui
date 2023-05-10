1b712b6e0931daf8a9cf967427b7c31e206da4c2
kgrozis <kgrozis@arista.com>
2023-05-09 20:27:13 -0400

    Update date in license

--- sample_network_tests/daemon/test_daemon.py
+++ sample_network_tests/daemon/test_daemon.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2019, Arista Networks EOS+
+# Copyright (c) 2023, Arista Networks EOS+
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
