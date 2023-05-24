ceb3c69e10e204d4c2827dcb982f1e21dd6aae83
Rewati Shitole <rewati@arista.com>
2023-05-24 14:18:32 -0700

    Reformatted test_steps.py

--- tests/unittests/fixtures/test_steps/test_steps.py
+++ tests/unittests/fixtures/test_steps/test_steps.py
@@ -7,17 +7,16 @@ Testcases for verification of syslog functionality
 
 
 from vane.logger import logger
-from vane.config import dut_objs, test_defs
-from vane import tests_tools
 
 TEST_SUITE = "baseline_mgmt_tests"
+
 
 class TestSyslogFunctionality:
     """
     Testcases for verification of syslog functionality
     """
 
-    def test_syslog_functionality_on_switch(self, dut, tests_definitions):
+    def test_syslog_functionality_on_switch(self):
         """TD: Testcase for verification of syslog logging server and
             source-interface information
 
@@ -25,28 +24,27 @@ class TestSyslogFunctionality:
             dut(dict): fixture used to iterate over all the duts
             tests_definitions(dict): test suite and test case parameters
         """
-        
+
         logger.info("This is a log")
 
         try:
             """
             TS: Running show logging command on dut and collecting the text output
             """
-            if True:
-                 logger.info("True")
+
+            logger.info("True")
 
             """
             TS: Checking logging source interface and Logging to string in output
             """
-           
-            assert True == True
+
+            assert True
 
             """
             TS: Collecting logging interface and IP address from command output
             """
-            
-            logger.info("This is a log")
 
+            logger.info("This is a log")
 
             """
             TS: Collecting information of syslog logging port and vrf details from command
@@ -56,15 +54,13 @@ class TestSyslogFunctionality:
 
         except (AssertionError, AttributeError, LookupError) as excep:
             logger.error(excep)
-           
 
         """
         TS: Comparing source-interface, port and vrf details from actual output
         with the expected output details
         """
-      
 
-    def test_syslog_functionality_on_server(self, dut, tests_definitions):
+    def test_syslog_functionality_on_server(self):
         """
         TD: Testcase for veification of syslog events on configured server
 
@@ -83,7 +79,7 @@ class TestSyslogFunctionality:
             TS: Running Tcpdump on syslog server and entering in config mode
             and existing to verify logging event are captured.
             """
-            assert True == True
+            assert True
 
         except (AttributeError, LookupError) as excep:
             logger.error(excep)
