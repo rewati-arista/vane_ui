d0aedb6829d178765218cada980814662cf74276
Gary Rybak <grybak@arista.com>
2023-08-09 17:41:33 -0600

    Revert change done for testing, and remove original coverage badge

--- vane/__init__.py
+++ vane/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, Arista Networks EOS+
+# Copyright (c) 2019, Arista Networks EOS+
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
