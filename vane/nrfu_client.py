d5de4e8528e448d72a2abe28019cf54ab7a9e572
Rewati Shitole <rewati@arista.com>
2023-08-15 10:54:48 -0700

    Removed pylint disables

--- vane/nrfu_client.py
+++ vane/nrfu_client.py
@@ -28,7 +28,6 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
-# pylint: disable=too-few-public-methods, unused-variable
 
 """Utilities for NRFU testing"""
 import getpass
