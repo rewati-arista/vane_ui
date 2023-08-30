e5ca2808c34a8977d428d7cbbee8dfa095bd7751
Vitthal Magadum <vitthal.magadum@gslab.com>
2023-08-30 14:07:32 +0530

    issue_424 Added testcase for interface status NRFU2.2

---
+++ nrfu_tests/test_interfaces_errdisabled.py
@@ -0,0 +1,87 @@
+# Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+
+"""Testcases for verification of Interface status functionality"""
+
+import pytest
+from pyeapi.eapilib import EapiError
+from vane import tests_tools
+from vane.logger import logger
+from vane.config import dut_objs, test_defs
+
+TEST_SUITE = "nrfu_tests"
+
+
+@pytest.mark.nrfu_test
+@pytest.mark.interfaces
+class InterfaceStatusTests:
+
+    """Testcases for verification of Interface status functionality"""
+
+    dut_parameters = tests_tools.parametrize_duts(TEST_SUITE, test_defs, dut_objs)
+    test_duts = dut_parameters["test_interfaces_errdisabled_status"]["duts"]
+    test_ids = dut_parameters["test_interfaces_errdisabled_status"]["ids"]
+
+    @pytest.mark.parametrize("dut", test_duts, ids=test_ids)
+    def test_interfaces_errdisabled_status(self, dut, tests_definitions):
+        """
+        TD: Test case for verification of interface errdisabled status
+        Args:
+          dut(dict): Details related to the switches
+          tests_definitions(dict): Test suite and test case parameters
+        """
+
+        tops = tests_tools.TestOps(tests_definitions, TEST_SUITE, dut)
+        self.output = ""
+        tops.expected_output = {"interface_state_details": {}}
+        tops.actual_output = {"interface_state_details": {}}
+
+        # Forming output message if test result is pass
+        tops.output_msg = "No interface is in error disabled state."
+
+        try:
+            """
+            TS: Running `show interfaces status errdisabled` command and verifying the
+            no interface is in error disabled state.
+            """
+            interface_details = dut["output"][tops.show_cmd]["json"]
+            logger.info(
+                "On device %s, Output of %s command is:\n%s\n",
+                tops.dut_name,
+                tops.show_cmd,
+                interface_details,
+            )
+            interface_statuses = interface_details.get("interfaceStatuses")
+            tops.expected_output["interface_state_details"] = {
+                "no_interface_in_error_disabled_state": True
+            }
+            tops.actual_output["interface_state_details"] = {
+                "no_interface_in_error_disabled_state": not bool(interface_statuses)
+            }
+
+            # forming output message if test result is fail
+            if tops.actual_output != tops.expected_output:
+                tops.output_msg = "\nFollowing interfaces are in errdisabled state:"
+                for interface in interface_statuses:
+                    causes = (
+                        str(interface_statuses.get(interface, {}).get("causes"))
+                        .lstrip("[")
+                        .rstrip("]")
+                    )
+                    description = interface_statuses.get(interface, {}).get("description")
+                    tops.output_msg += (
+                        f"\n{interface},\nDescription: {description} and Causes: {causes}"
+                    )
+
+        except (AssertionError, AttributeError, LookupError, EapiError) as excep:
+            tops.actual_output = tops.output_msg = str(excep).split("\n", maxsplit=1)[0]
+            logger.error(
+                "On device %s: Error while running the testcase is:\n%s",
+                tops.dut_name,
+                tops.actual_output,
+            )
+
+        tops.test_result = tops.expected_output == tops.actual_output
+        tops.parse_test_steps(self.test_interfaces_errdisabled_status)
+        tops.generate_report(tops.dut_name, self.output)
+        assert tops.expected_output == tops.actual_output
