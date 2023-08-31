7f00b3e4a2fc7603c3718e7efa2c8668944ebbde
DhruvGoel <GS-3063@GSLAB.COM>
2023-09-01 00:00:57 +0530

    Issue_430: Resolved conflicts

---
+++ nrfu_tests/test_routing_bgp_evpn_peers_state.py
@@ -0,0 +1,123 @@
+# Copyright (c) 2023 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+
+"""
+Test cases for verification of routing BGP EVPN functionality.
+"""
+
+import pytest
+from pyeapi.eapilib import EapiError, CommandError
+from vane.logger import logger
+from vane.config import dut_objs, test_defs
+from vane import tests_tools
+
+TEST_SUITE = "nrfu_tests"
+
+
+@pytest.mark.nrfu_test
+@pytest.mark.routing
+class BgpEvpnTests:
+    """
+    Test cases for verification of routing BGP EVPN functionality.
+    """
+
+    dut_parameters = tests_tools.parametrize_duts(TEST_SUITE, test_defs, dut_objs)
+    test_duts = dut_parameters["test_routing_bgp_evpn_peers_state"]["duts"]
+    test_ids = dut_parameters["test_routing_bgp_evpn_peers_state"]["ids"]
+
+    @pytest.mark.parametrize("dut", test_duts, ids=test_ids)
+    def test_routing_bgp_evpn_peers_state(self, dut, tests_definitions):
+        """
+        TD: Test case for verification of routing BGP EVPN functionality.
+        Args:
+            dut(dict): details related to a particular DUT
+            tests_definitions(dict): test suite and test case parameters.
+        """
+
+        # Creating Testops class object and initializing the variables
+        tops = tests_tools.TestOps(tests_definitions, TEST_SUITE, dut)
+        self.output = ""
+        tops.actual_output = {"bgp_peers": {}}
+        tops.expected_output = {"bgp_peers": {}}
+        input_parameters = tops.test_parameters["input"]
+        skip_on_command_unavailable = input_parameters["skip_on_command_unavailable"]
+        evpn_output = {}
+
+        # Forming output message if test result is passed
+        tops.output_msg = "All BGP EVPN peers state is established."
+
+        try:
+            """
+            TS: Running `show bgp evpn summary` command on device and verifying all the BGP EVPN
+            peers state is established.
+            """
+
+            try:
+                evpn_output = dut["output"][tops.show_cmd]["json"]
+
+            except CommandError:
+                # Checking for the condition whether to skip the test case or
+                # check the assert statement
+                if skip_on_command_unavailable:
+                    pytest.skip(
+                        f"On device {tops.dut_name}, command is unavailable, device might be in"
+                        " ribd mode."
+                    )
+                else:
+                    assert False, (
+                        f"On device {tops.dut_name}, command is unavailable, device might be in"
+                        " ribd mode."
+                    )
+
+            logger.info(
+                "On device %s, output of %s command is:\n%s\n",
+                tops.dut_name,
+                tops.show_cmd,
+                evpn_output,
+            )
+            self.output += (
+                f"On device {tops.dut_name}, output of {tops.show_cmd} command is:\n{evpn_output}\n"
+            )
+            assert evpn_output, "BGP EVPN output is not found."
+
+            bgp_evpn_neighbors = evpn_output.get("vrfs")
+            assert bgp_evpn_neighbors, "VRFs details are not found in the output."
+
+            # Collecting peer details and forming expected and actual output dictionaries
+            # with the peer state
+            for vrf in bgp_evpn_neighbors:
+                bgp_evpn_peers = bgp_evpn_neighbors.get(vrf, {}).get("peers")
+                assert bgp_evpn_peers, f"For VRF {vrf}, BGP EVPN peers are not configured."
+
+                for peer in bgp_evpn_peers:
+                    peer_state = bgp_evpn_peers.get(peer, {}).get("peerState")
+                    tops.expected_output["bgp_peers"].update({peer: {"peer_state": "Established"}})
+                    tops.actual_output["bgp_peers"].update({peer: {"peer_state": peer_state}})
+
+            # Forming output message if test result is failed
+            if tops.expected_output != tops.actual_output:
+                tops.output_msg = ""
+                for peer, peer_state in tops.expected_output["bgp_peers"].items():
+                    actual_peer_state = (
+                        tops.actual_output.get("bgp_peers", {}).get(peer, {}).get("peer_state")
+                    )
+                    expected_peer_state = peer_state.get("peer_state")
+                    if actual_peer_state != expected_peer_state:
+                        tops.output_msg += (
+                            f"\nFor BGP EVPN peer {peer}, expected state is"
+                            f" '{expected_peer_state}', however actual is found as"
+                            f" '{actual_peer_state}'."
+                        )
+
+        except (AssertionError, AttributeError, LookupError, EapiError) as excep:
+            tops.output_msg = tops.actual_output = str(excep).split("\n", maxsplit=1)[0]
+            logger.error(
+                "On device %s, Error while running the testcase is:\n%s",
+                tops.dut_name,
+                tops.actual_output,
+            )
+
+        tops.test_result = tops.expected_output == tops.actual_output
+        tops.parse_test_steps(self.test_routing_bgp_evpn_peers_state)
+        tops.generate_report(tops.dut_name, self.output)
+        assert tops.expected_output == tops.actual_output
